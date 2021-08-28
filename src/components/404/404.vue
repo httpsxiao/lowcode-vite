179b174f9fdeb575bb16d5055fbb7d4a4402494d
httpsxiao <day_clear@163.com>
2021-08-28 14:56:17 +0800

    update

---
+++ src/components/404/404.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div class="tip">404</div>
+    <div class="btn">
+      <el-button type="primary" plain @click="goIndex">回到首页</el-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, computed } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default defineComponent(() => {
+  const state = reactive({ msg: '404' })
+  const computedMsg = computed(() => state.msg + '~')
+  const router = useRouter()
+  const goIndex = () => router.replace('/')
+
+  return { computedMsg, goIndex }
+})
+</script>
+
+<style scoped lang="stylus">
+.container
+  margin-top: 100px;
+  .tip
+    font-size 40px
+  .btn
+    margin-top 20px
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-container>
+    <el-header height="100px"></el-header>
+    <el-container>
+      <el-aside width="300px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+    <el-footer height="50px"></el-footer>
+  </el-container>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Index',
+  setup: () => {
+    const count = ref(0)
+    return { count }
+  }
+})
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
