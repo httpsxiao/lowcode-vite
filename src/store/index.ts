f738f30550e8136ce9d59169cc5bbaa6f071dc84
httpsxiao <day_clear@163.com>
2021-09-27 14:43:11 +0800

    update store

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,9 @@ export interface ProjectDataIns {
   scaleX: number,
   scaleY: number,
   width: number,
-  height: number
+  height: number,
+  elements: any[],
+  activeElementUuid: string
 }
 
 export interface State {
@@ -27,18 +29,40 @@ export default createStore<State>({
         scaleX: 1,
         scaleY: 1,
         width: 375,
-        height: 500
+        height: 500,
+        elements: [],
+        activeElementUuid: ''
       }
     }
   },
   getters: {
-    double(state) {
-      return 2 * state.count
+    activeElementUuid(state) {
+      return state.projectData.activeElementUuid
     }
   },
   mutations: {
     setState(state, { key, value }) {
       state[key as keyof State] = value
+    },
+    addElement(state, element) {
+      state.projectData.elements.push(element)
+    },
+    setActiveElementUuid(state, uuid = '') {
+      state.projectData.activeElementUuid = uuid
+    },
+    setElementPosition(state, data: SetElementPositionIns) {
+      const { uuid, top, left } = data
+      const elements = state.projectData.elements
+      for (let i = 0; i < elements.length; i++) {
+        if (uuid === elements[i].uuid) {
+          elements[i].style = {
+            ...elements[i].style,
+            top,
+            left
+          }
+          break
+        }
+      }
     }
   },
   actions: {
